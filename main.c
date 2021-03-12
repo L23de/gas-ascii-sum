837fbf2145f2ea40e83fba1e6db1ad38cfec88b5
Lester Huang <leh223@lehigh.edu>
2021-03-12 10:20:34 -0500

    Added a test for when no input is entered

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 extern int assem(int*, char**);
 
-int main(int argc, char** argv) {
-    fprintf(stdout, "String\t\t\t\tASCII Value");
-    fprintf(stdout, "----------------------------------------------------------------------");
-
-    for (int i = 0; i < argc; i++) {
-        fprintf(stdout, "%s\t\t\t", argv[i]);
-        fprintf(stdout, "%d\n", atoi(argv[i]));
+// Finds the sum of the ASCII values composed in string
+int printASCII(char* string) {
+    int adder = 0;
+    for (int i = 0; i < strlen(string); i++) {
+        int charVal = string[i];
+        adder += charVal;
     }
-    fprintf(stdout, "----------------------------------------------------------------------");
-    fprintf(stdout, "%d", assem(&argc, argv));
+    return adder;
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        fprintf(stdout, "No input detected. Re-run with value as cmd-line args");
+    }
+
+    fprintf(stdout, "String\t\t\tASCII Value\n");
+    fprintf(stdout, "----------------------------------------------------\n");
+
+    for (int i = 1; i < argc; i++) {
+        fprintf(stdout, "%-10s\t\t", argv[i]);
+        fprintf(stdout, "%-10d\n", printASCII(argv[i]));
+    }
+
+    fprintf(stdout, "----------------------------------------------------\n");
+    fprintf(stdout, "Sum\t\t\t%-d\n", assem(&argc, argv));
 }
